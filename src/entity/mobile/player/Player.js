e718f32b889240a128cbbc4022c3ff57c984e012
happ-2h <hikonetgames@gmail.com>
2024-09-07 18:40:02 -0700

    Time based movement

--- src/entity/mobile/player/Player.js
+++ src/entity/mobile/player/Player.js
@@ -6,7 +6,7 @@ export default class Player extends Entity_Mob {
   constructor(x=0, y=0) {
     super(x, y, new PlayerController);
 
-    this.vel.set(1, 1);
+    this.vel.set(50, 50);
   }
 
   init() {}
@@ -19,29 +19,40 @@ export default class Player extends Entity_Mob {
   #handleInput() {
     if (!this.isMoving && this.controller.isRequestingLeft()) {
       this.targetTile.x -= TILE_SIZE;
+      this.dir.x = -1;
       this.isMoving = true;
     }
     else if (!this.isMoving && this.controller.isRequestingRight()) {
       this.targetTile.x += TILE_SIZE;
+      this.dir.x = 1;
       this.isMoving = true;
     }
     else if (!this.isMoving && this.controller.isRequestingUp()) {
       this.targetTile.y -= TILE_SIZE;
+      this.dir.y = -1;
       this.isMoving = true;
     }
     else if (!this.isMoving && this.controller.isRequestingDown()) {
       this.targetTile.y += TILE_SIZE;
+      this.dir.y = 1;
       this.isMoving = true;
     }
   }
 
   #handleMovement(dt) {
     // Finalize movement
-    if (this.targetTile.x > this.dst.x) this.dst.x += this.vel.x;
-    if (this.targetTile.x < this.dst.x) this.dst.x -= this.vel.x;
-    if (this.targetTile.y > this.dst.y) this.dst.y += this.vel.y;
-    if (this.targetTile.y < this.dst.y) this.dst.y -= this.vel.y;
+    if (this.dir.x ===  1 && this.targetTile.x > this.dst.x) this.dst.x += this.vel.x * dt;
+    if (this.dir.x === -1 && this.targetTile.x < this.dst.x) this.dst.x -= this.vel.x * dt;
+    if (this.dir.y ===  1 && this.targetTile.y > this.dst.y) this.dst.y += this.vel.y * dt;
+    if (this.dir.y === -1 && this.targetTile.y < this.dst.y) this.dst.y -= this.vel.y * dt;
 
-    if (this.dst.pos.equals(this.targetTile)) this.isMoving = false;
+    if (
+      Math.floor(this.dst.x) === this.targetTile.x &&
+      Math.floor(this.dst.y) === this.targetTile.y
+    ) {
+      this.isMoving = false;
+      this.dir.x = 0;
+      this.dir.y = 0;
+    }
   }
 };
